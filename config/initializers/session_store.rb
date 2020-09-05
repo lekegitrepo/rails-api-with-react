02a7be7a9561acef9fd07e26bf1f596bdc00e344
lekegitrepo <lekedevrepo@gmail.com>
2020-09-05 13:45:49 -0500

    add herokuapp.com to the domain name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_authen_app', domain: 'rails-api-with-react-auth'
+Rails.application.config.session_store :cookie_store, key: '_authen_app', domain: 'rails-api-with-react-auth.herokuapp.com'
