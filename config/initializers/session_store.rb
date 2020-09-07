2da106e56ec1af13eaea0d9c0c48d34937ab620a
lekegitrepo <lekedevrepo@gmail.com>
2020-09-07 06:25:46 -0500

    configure authentication to work in development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_authen_app', domain: 'rails-api-with-react-auth.herokuapp.com'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_authen_app', domain: 'rails-api-with-react-auth.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_authen_app'
+end
