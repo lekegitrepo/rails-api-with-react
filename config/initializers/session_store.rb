1231bbef1260be32bb9138842828a9dbb1766adf
lekegitrepo <lekedevrepo@gmail.com>
2020-10-15 09:56:47 -0500

    rename the domain name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_authen_app', domain: 'rails-api-with-react-auth.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_authen_app', domain: 'railsapiwithreactauth.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_authen_app'
 end
