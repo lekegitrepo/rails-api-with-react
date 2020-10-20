84f4ab2d1371476177813c2df8fd49cf5928294b
lekegitrepo <lekedevrepo@gmail.com>
2020-10-20 08:06:06 -0500

    add a new url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,4 +8,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'https://rails-react-authen-app.netlify.app'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
+
+  allow do
+    origins 'https://localmat.netlify.app'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 end
